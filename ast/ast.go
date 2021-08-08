2978fa6886e4cf9d082284d64e883e93b40b6b72
soukouki <soukouki0@yahoo.co.jp>
2021-08-09 00:30:48 +0900

    ExpressionStatementとString

--- ast/ast.go
+++ ast/ast.go
@@ -2,10 +2,12 @@ package ast
 
 import (
 	"yokan/token"
+	"bytes"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -30,14 +32,47 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type AssignStatement struct {
 	Name *Identifier
 	Value Expression
 }
 
 func (as *AssignStatement) statementNode() { }
-
 func (as *AssignStatement) TokenLiteral() string {
+	return ""
+}
+
+func (as *AssignStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(as.Name.String()+" = ")
+	if as.Value != nil {
+		out.WriteString(as.Value.String())
+	}
+	out.WriteString("\n")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() { }
+func (es *ExpressionStatement) TokenLiteral() string {
+	return ""
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
 	return ""
 }
 
@@ -47,7 +82,10 @@ type Identifier struct {
 }
 
 func (i *Identifier) expressionNode() { }
-
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
